4e197896b583d0883a8a1ba7304e96abe95ef427
geunwoo <960722rjsdn@naver.com>
2020-10-05 13:19:46 +0900

    Coding_test

--- 월간코딩챌린지시즌1/문제3.cpp
+++ 월간코딩챌린지시즌1/문제3.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <limits.h>
 
 using namespace std;
 vector <int> answer_a;
@@ -10,41 +11,33 @@ vector <int> answer_a;
 int solution(vector<int> a) {
     int answer = 0;
 
-    int min_num = INT_MAX, min_index;
-    for(int i = 0; i < a.size(); i ++){
-        if(min_num > a[i]) {
-            min_num = a[i];
-            min_index = i;
-        }
+    answer = 2; // 처음과 끝은 무조건 가능
+
+    vector<pair<int, int> > a_index;
+
+    for(int i = 0; i < a.size(); i++) {
+        // vector a의 값과 index값을 같이 가지고 있는 vector를 만들어 줌
+        a_index.push_back(pair<int, int>(a[i], i));
     }
 
-    answer = 2; // 처음과 끝은 무조건 가능
-    for(int i = 1; i < a.size() - 1; i++) {
-        int possible = true;
-        if(i < min_index) {
-            for(int j = 0; j < i; j++) {
-                if(a[j] < a[i]){
-                    possible = false;
-                    break;
-                }
-            }
-        }
-        else if(i > min_index) {
-            for(int j = i + 1; j < a.size(); j++) {
-                if(a[j] < a[i]) {
-                    possible = false;
-                    break;
-                }
-            }
-        }
-        else {// 최소인 경우
-            answer += 1;
-            continue;
-        }
+    // a_index를 오름차순으로 정렬 
+    sort(a_index.begin(), a_index.end());
 
-    if(possible == true)
-        answer += 1;
+    int start = min(a_index[0].second, a_index[1].second);
+    int end = max(a_index[0].second, a_index[1].second);
 
+    // 3번째 작은 값부터 최후에 남길 수 있는 풍선인지 확인 
+    for(int i = 2; i < a_index.size(); i++) {
+        if(a_index[i].second > start && a_index[i].second < end) {// 자신의 왼쪽, 오른쪽에 자신보다 작은 값이 존재할 때 (불가능)
+
+        }
+        else { // 한쪽에만 자신보다 작은 값이 존재할 때 
+            ++answer;
+            if(a_index[i].second > end)
+                end = a_index[i].second;
+            else 
+                start = a_index[i].second;
+        }
     }
 
     return answer;
@@ -54,16 +47,18 @@ int solution(vector<int> a) {
 int main() {
     vector <int> a;
 
-    a.push_back(-16);
-    a.push_back(27);
-    a.push_back(65);
-    a.push_back(-2);
-    a.push_back(58);
-    a.push_back(-92);
-    a.push_back(-71);
-    a.push_back(-68);
-    a.push_back(-61);
-    a.push_back(-33);
+    a.push_back(<int>(-16, 27, 65, -2, 58, -92, -71, -68, -61, -33));
+
+    // a.push_back(-16);
+    // a.push_back(27);
+    // a.push_back(65);
+    // a.push_back(-2);
+    // a.push_back(58);
+    // a.push_back(-92);
+    // a.push_back(-71);
+    // a.push_back(-68);
+    // a.push_back(-61);
+    // a.push_back(-33);
 
 
     int answer = solution(a);
