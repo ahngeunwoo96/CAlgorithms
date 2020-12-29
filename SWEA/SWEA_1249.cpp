282f3dd9a121b178765ead6583c0b61fa4198708
geunwoo <960722rjsdn@naver.com>
2020-12-29 21:59:30 +0900

    20.12.29 SWEA 1249 문제풀이

--- SWEA/SWEA_1249.cpp
+++ SWEA/SWEA_1249.cpp
@@ -1,39 +1,17 @@
-// 20.12.28 SWEA 1249 보급로 문제풀이
+// 20.12.29 SWEA 1249 보급로 문제풀이
 
 #include <iostream>
-#include <limits.h>
 #include <stdio.h>
+#include <queue>
+#include <limits.h>
 
 using namespace std;
 
-int map[100][100], sumMap[100][100];
-int N, answer;
-
-void findBestWay(int sum, int i, int j) {
-
-    if(i == N - 1 && j == N - 1) {
-        if(answer > sum) {
-            answer = sum;
-        }
-        return;
-    }
-    
-    if(sum >= answer) {
-        return;
-    }
-
-    if(sum < sumMap[i][j]) {
-        sumMap[i][j] = sum;
-    }
-
-    if(sum > sumMap[i][j])
-        return;
-    
-    if(j + 1 < N)
-        findBestWay(sum + map[i][j + 1], i, j + 1);
-    if(i + 1 < N)
-        findBestWay(sum + map[i + 1][j], i + 1, j);
-}
+int map[100][100];
+int sumMap[100][100];
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+queue < pair<int, int>> q;
 
 int main() {
 
@@ -41,25 +19,39 @@ int main() {
     cin >> T;
 
     for(int test_case = 1; test_case <= T; test_case++) {
-        answer = INT_MAX;
+        int N;
         cin >> N;
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                map[i][j] = 0;
+                scanf("%1d", &map[i][j]);
                 sumMap[i][j] = INT_MAX;
             }
         }
 
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                scanf("%1d" , &map[i][j]);
+        sumMap[0][0] = 0;
+        q.push(make_pair(0, 0));
+
+        while(!q.empty()) {
+            int x = q.front().first;
+            int y = q.front().second;
+            q.pop();
+            for(int i = 0; i < 4; i++) {
+                int xx = x + dx[i];
+                int yy = y + dy[i];
+                if(xx >= 0 && xx < N && yy >= 0 && yy < N) {
+                    if(sumMap[xx][yy] > sumMap[x][y] + map[xx][yy]) {
+                        sumMap[xx][yy] = sumMap[x][y] + map[xx][yy];
+                        q.push({xx, yy});
+                    }
+                }
+
             }
         }
 
-        findBestWay(0, 0, 0);
+        
 
-        cout << "#" << test_case << " " << answer << endl;
+        cout << "#" << test_case << " " << sumMap[N - 1][N - 1] << endl;
 
     }
 
