e4945201606f2bc4727dadc122e3f4df0cf33ec9
ahngeunwoo96 <960722rjsdn@naver.com>
2021-01-28 22:33:16 +0900

    create

---
+++ SWEA/SWEA_1983.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# 21.1.19 SWEA 조교의 성적 매기기 python
+
+# T = int(input())
+
+# for test_Case in range(1, T + 1):
+
+#     St_num, K = int(input()).split()
+
+#     for i in range(St_num):
+#         mid.append(input())
+#         fin.append(input())
+#         hom.append(input())
+
+#     print(mid)
+#     print(fin)
+#     print(hom)
+
+St_num, K = map(int, input().split())
+
+print(St_num)
+print(K)
